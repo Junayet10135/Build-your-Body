ce3b993cf554b3410213ee786aa2fc7a95d9f22c
JunayetTamim <junayettamim186@gmail.com>
2022-04-16 22:22:41 +0600

    folder structure ready

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='container'>
+            <h2>This is Header</h2>
+        </div>
+    );
+};
+
+export default Header;
