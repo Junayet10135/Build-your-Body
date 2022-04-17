2db208b7ec8766aec7542aaadc04c0464e8aaae1
JunayetTamim <junayettamim186@gmail.com>
2022-04-17 19:57:12 +0600

    checkout page update

--- src/Pages/CheckOut/CheckOut.js
+++ src/Pages/CheckOut/CheckOut.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { toast, ToastContainer } from 'react-toastify';
+
 
 const CheckOut = () => {
+
+
+    const handleCheckOut = async(event)=>{
+        event.preventDefault();
+        toast('Your form Submitted');
+
+    }
     return (
-        <div>
-            <h2>Hello</h2>
+        <div className='container mx-auto w-50 mt-5 form-container'>
+            <h2 className='text-center'>Checkout</h2>
+            <Form onSubmit={handleCheckOut}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Your Name</Form.Label>
+                    <Form.Control type="name" placeholder="Enter Your Name" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Phone Number</Form.Label>
+                    <Form.Control type="number" placeholder="Enter Mobile Number" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Your Address</Form.Label>
+                    <Form.Control type="text" placeholder="Enter Your Address" required />
+                </Form.Group>
+                <Button className='d-block w-50 mx-auto my-2' variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+            <ToastContainer />
         </div>
     );
 };
